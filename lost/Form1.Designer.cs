eba9e388eeb1cc932b1b6f35546274374f2cf364
scottythornton <scotthor086@ed.amdsb.ca>
2018-11-09 15:14:54 -0500

    Add project files.

---
+++ lost/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace lost
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.outputLabel1 = new System.Windows.Forms.Label();
+            this.blueLabel = new System.Windows.Forms.Label();
+            this.redLabel = new System.Windows.Forms.Label();
+            this.storyPictures = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.storyPictures)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // outputLabel1
+            // 
+            this.outputLabel1.AutoSize = true;
+            this.outputLabel1.Font = new System.Drawing.Font("MV Boli", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel1.Location = new System.Drawing.Point(2, 23);
+            this.outputLabel1.Name = "outputLabel1";
+            this.outputLabel1.Size = new System.Drawing.Size(58, 25);
+            this.outputLabel1.TabIndex = 0;
+            this.outputLabel1.Text = "label1";
+            // 
+            // blueLabel
+            // 
+            this.blueLabel.AutoSize = true;
+            this.blueLabel.Font = new System.Drawing.Font("Trajan Pro", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blueLabel.Location = new System.Drawing.Point(98, 372);
+            this.blueLabel.Name = "blueLabel";
+            this.blueLabel.Size = new System.Drawing.Size(65, 20);
+            this.blueLabel.TabIndex = 1;
+            this.blueLabel.Text = "label1";
+            // 
+            // redLabel
+            // 
+            this.redLabel.AutoSize = true;
+            this.redLabel.Font = new System.Drawing.Font("Trajan Pro", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.redLabel.Location = new System.Drawing.Point(98, 304);
+            this.redLabel.Name = "redLabel";
+            this.redLabel.Size = new System.Drawing.Size(65, 20);
+            this.redLabel.TabIndex = 2;
+            this.redLabel.Text = "label2";
+            // 
+            // storyPictures
+            // 
+            this.storyPictures.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.storyPictures.Location = new System.Drawing.Point(132, 103);
+            this.storyPictures.Name = "storyPictures";
+            this.storyPictures.Size = new System.Drawing.Size(211, 165);
+            this.storyPictures.TabIndex = 7;
+            this.storyPictures.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::lost.Properties.Resources.blue_50x50;
+            this.pictureBox2.Location = new System.Drawing.Point(42, 358);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::lost.Properties.Resources.red_50x50;
+            this.pictureBox1.Location = new System.Drawing.Point(42, 287);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(521, 424);
+            this.Controls.Add(this.storyPictures);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.redLabel);
+            this.Controls.Add(this.blueLabel);
+            this.Controls.Add(this.outputLabel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Lost";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.storyPictures)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel1;
+        private System.Windows.Forms.Label blueLabel;
+        private System.Windows.Forms.Label redLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox storyPictures;
+    }
+}
+
---
+++ lost/Form1.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lost
+{
+    public partial class Form1 : Form
+    {
+        int scene = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            outputLabel1.Text = "Your plane crashed on a island and you are the only " +
+                "\nsurvivor";
+            redLabel.Text = "Search the plane";
+            blueLabel.Text = "Go out and scavenge";
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.R)
+            {
+                if (scene == 0)
+                {
+                    scene = 1;
+                }
+                else if (scene == 1)
+                {
+                    scene = 3;
+                }
+                else if (scene == 2)
+                {
+                    scene = 0;
+                }
+                else if (scene == 99)
+                {
+                    this.Close();
+                }
+                else if (scene == 3)
+                {
+                    scene = 5;
+                }
+                else if (scene == 5)
+                {
+                    scene = 6;
+                }
+                else if (scene == 4)
+                {
+                    scene = 0;
+                }
+                else if (scene == 7)
+                {
+                    scene = 8;
+                }
+                else if (scene == 10)
+                {
+                    scene = 11;
+                }
+                else if (scene == 12)
+                {
+                    scene = 14;
+                }
+                else if (scene == 13)
+                {
+                    scene = 16;
+                }
+                else if (scene == 16)
+                {
+                    scene = 18;
+                }
+                else if (scene == 8)
+                {
+                    scene = 0;
+                }
+                else if (scene == 6)
+                {
+                    scene = 0;
+                }
+                else if (scene == 18)
+                {
+                    scene = 19;
+                }
+                else if (scene == 14)
+                {
+                    scene = 0;
+                }
+                else if (scene == 11)
+                {
+                    scene = 0;
+                }
+                else if (scene == 9)
+                {
+                    scene = 0;
+                }
+                else if (scene == 15)
+                {
+                    scene = 0;
+                }
+                else if (scene == 17)
+                {
+                    scene = 0;
+                }
+                else if (scene == 19)
+                {
+                    scene = 0;
+                }
+                else if (scene == 20)
+                {
+                    scene = 0;
+                }
+            }
+            else if (e.KeyCode == Keys.B)
+            {
+                if (scene == 0)
+                {
+                    scene = 10;
+                }
+                else if (scene == 8)
+                {
+                    scene = 99;
+                }
+                else if (scene ==2)
+                {
+                    scene = 99;
+                }
+                else if (scene == 3)
+                {
+                    scene = 4;
+                }
+                else if (scene ==5)
+                {
+                    scene = 7;
+                }
+                else if (scene == 1)
+                {
+                    scene = 2;
+                }
+                else if (scene == 7)
+                {
+                    scene = 9;
+                }
+                else if (scene == 6)
+                {
+                    scene = 99;
+                }
+                else if (scene == 10)
+                {
+                    scene = 12;
+                }
+                else if (scene == 11)
+                {
+                    scene = 99;
+                }
+                else if (scene == 12)
+                {
+                    scene = 13;
+                }
+                else if (scene == 9)
+                {
+                    scene = 99;
+                }
+                else if (scene == 13)
+                {
+                    scene = 15;
+                }
+                else if (scene == 4)
+                {
+                    scene = 99;
+                }
+                else if (scene == 16)
+                {
+                    scene = 17;
+                }
+                else if (scene == 15)
+                {
+                    scene = 99;
+                }
+                else if (scene == 14)
+                {
+                    scene = 99;
+                }
+                else if (scene == 17)
+                {
+                    scene = 99;
+                }
+                else if (scene == 19)
+                {
+                    scene = 99;
+                }
+                else if (scene == 20)
+                {
+                    scene = 99;
+                }
+                else if (scene == 18)
+                {
+                    scene = 20;
+                }
+            }
+            switch (scene)
+            {
+                case 0:
+                    storyPictures.BackgroundImage = null;
+                    outputLabel1.Text = "Your plane crashed on a island and you are the only " +
+                        "\nsurvivor";
+                    redLabel.Text = "Search the plane";
+                    blueLabel.Text = "Go out and scavenge";
+                    break;
+                case 1:
+                    outputLabel1.Text = "You find what seems to be water";
+                    redLabel.Text = "Leave it";
+                    blueLabel.Text = "Drink it";
+                    break;
+                case 2:
+                    outputLabel1.Text = "It's water mixed with a odorless chemical and you " +
+                        "\ndie";
+                    redLabel.Text = "Play Again";
+                    blueLabel.Text = "End";
+                    break;
+                case 3:
+                    outputLabel1.Text = "You keep looking and find some food the flight " +
+                        "\nattendants were serving";
+                    redLabel.Text = "Take it";
+                    blueLabel.Text = "Leave it";
+                    break;
+                case 4:
+                    outputLabel1.Text = "You die of starvation because you couldn't find " +
+                        "\nany other food";
+                    redLabel.Text = "Play Again";
+                    blueLabel.Text = "End";
+                    break;
+                case 5:
+                    outputLabel1.Text = "The food is good. You are stuffed. It's now night " +
+                        "\ntime";
+                    redLabel.Text = "Sleep on the plane";
+                    blueLabel.Text = "Find shelter for the night";
+                    break;
+                case 6:
+                    outputLabel1.Text = "There is still a fire somewhere on the plane and " +
+                        "\nthe flame comes in contact with gas, and the plane blows up and " +
+                        "\nkills you";
+                    redLabel.Text = "Play Again";
+                    blueLabel.Text = "End";
+                    break;
+                case 7:
+                    outputLabel1.Text = "You find a good place to stay for the night. It's " +
+                        "\nthe next morning";
+                    redLabel.Text = "Set up camp";
+                    blueLabel.Text = "Call out SOS";
+                    break;
+                case 8:
+                    outputLabel1.Text = "You miss a boat coming by because you were too busy " +
+                        "\nsetting up camp and will never be rescued";
+                    redLabel.Text = "Play Again";
+                    blueLabel.Text = "End";
+                    break;
+                case 9:
+                    outputLabel1.Text = "A rescue boat comes and saves you and you are taken " +
+                        "\nhome to your family, YOU WIN!";
+                    redLabel.Text = "Play Again";
+                    blueLabel.Text = "End";
+                    storyPictures.BackgroundImage = Properties.Resources.boat; ;
+                    break;
+                case 10:
+                    outputLabel1.Text = "You find a bush that is rattiling around.";
+                    redLabel.Text = "Go investigate the bush";
+                    blueLabel.Text = "Go the other way";
+                    storyPictures.BackgroundImage = Properties.Resources.bush;
+                    break;
+                case 11:
+                    outputLabel1.Text = "It's a family of black mamba's and they all jump out " +
+                        "\nand feed off you";
+                    redLabel.Text = "Play Again";
+                    blueLabel.Text = "End";
+                    storyPictures.BackgroundImage = Properties.Resources.snake;
+                    break;
+                case 12:
+                    storyPictures.BackgroundImage = null;
+                    outputLabel1.Text = "You find shelter and food to keep you safe for a few " +
+                        "\ndays, but you need water";
+                    redLabel.Text = "Go North";
+                    blueLabel.Text = "Go South";
+                    break;
+                case 13:
+                    outputLabel1.Text = "You find a lake but hear people yelling in the distance";
+                    redLabel.Text = "Go towards the voices";
+                    blueLabel.Text = "Run away";
+                    break;
+                case 14:
+                    outputLabel1.Text = "You run into a hippo and it eats you, you are now dead";
+                    redLabel.Text = "Play Again";
+                    blueLabel.Text = "End";
+                    storyPictures.BackgroundImage = Properties.Resources.hippo;
+                    break;
+                case 15:
+                    outputLabel1.Text = "You run into a jaguar and die instantly";
+                    redLabel.Text = "Play Again";
+                    blueLabel.Text = "End";
+                    storyPictures.BackgroundImage = Properties.Resources.jaguar;
+                    break;
+                case 16:
+                    outputLabel1.Text = "You see a huge village with a lot of people with swords " +
+                        "\nand spears";
+                    redLabel.Text = "Sneak into the main building";
+                    blueLabel.Text = "Try and talk to them";
+                    storyPictures.BackgroundImage = Properties.Resources.village;
+                    break;
+                case 17:
+                    storyPictures.BackgroundImage = null;
+                    outputLabel1.Text = "They get scared and you get captured and eaten alive, " +
+                        "\nyou are now dead.";
+                    redLabel.Text = "Play Again";
+                    blueLabel.Text = "End";
+                    break;
+                case 18:
+                    storyPictures.BackgroundImage = null;
+                    outputLabel1.Text = "You see the people doing a ritual but you knock " +
+                        "\nsomething over and you are found and captured";
+                    redLabel.Text = "Ask to be apart of their tribe";
+                    blueLabel.Text = "Take the offer to be sent home in a boat";
+                    break;
+                case 19:
+                    outputLabel1.Text = "They betray you and they kill you and eat you.";
+                    redLabel.Text = "Play Again";
+                    blueLabel.Text = "End";
+                    break;
+                case 20:
+                    outputLabel1.Text = "You take a boat home and are reunited with your family " +
+                        "\nYOU WIN!";
+                    redLabel.Text = "Play Again";
+                    blueLabel.Text = "End";
+                    break;
+                case 99:
+                    this.Close();
+                    break;
+            }
+        }
+    }
+}
+    
